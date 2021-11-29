84b0679bf0cd63f583b6b620c6c4659453c1a202
Jean Leconte II <jeanemileleconteii@gmail.com>
2021-11-29 16:13:14 -0600

    not found component tests pass

---
+++ src/__tests__/NotFound.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import NotFound from '../components/NotFound';
+
+
+const mockStore = configureStore([]);
+
+describe('check 404 page properties', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      myState: 'hello',
+    });
+  });
+
+
+  test('check for text', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NotFound />
+        </BrowserRouter>
+      </Provider>
+    );
+    const titleElement = screen.getByText(/Resource Not Found/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+});
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,13 @@
 /* 404 page */
-// make sure you practice adding an image in here -> storage it in an Azure blob
 import React from 'react';
 import '../styles/NotFound.css';
 import { Link } from 'react-router-dom';
 
+// TODO: make sure you practice adding an image in here -> storage it in an Azure blob
 const NotFound = () => {
   return (
     <>
-      <p className="not-found"> placeholder text </p>
+      <p className="not-found"> Resource Not Found </p>
       <Link to="/"> Return to home screen </Link>
     </>
   );
